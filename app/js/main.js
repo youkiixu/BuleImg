44d0d31d068d5afc5d20b70c538aa8ae2c776a99
lllidan120 <lllidan120@163.com>
2018-11-05 18:47:37 +0800

    '项目脚手架'

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// var $ = require('jquery');
+
+// 使用 Amaze UI 源码中的模块
+// var addToHome = require('amazeui/js/ui.add2home');
+
+// 使用 NPM 中的模块
+// var hljs = require('highlight.js');
+
+$(function() {
+  
+
+  
+});
