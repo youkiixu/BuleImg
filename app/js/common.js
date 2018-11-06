2c5c44ce12f8ec83ed6f21d3c54c96f550dc231d
zhang <zhangxuejuan@kiy.cn>
2018-11-06 17:43:53 +0800

    'zxj'

--- app/js/common.js
+++ app/js/common.js
@@ -3,18 +3,18 @@
 $(function(){
     //分类的隐藏与显示
      $(".header_classifi_item").mouseover(function(){
-        $(".header_category").hide();  //隐藏
+        $(".header_category").show();  //隐藏
     });
     $(".header_classifi_item").mouseout(function(){
-        $(".header_category").show();  //显示
+        $(".header_category").hide();  //显示
     });
 
     //个人中心的隐藏与显示
     $(".login_success_img").mouseover(function(){
-        $(".order_info").hide();  //隐藏
+        $(".order_info").show();  //隐藏
     });
     $(".login_success_img").mouseout(function(){
-        $(".order_info").show();  //显示
+        $(".order_info").hide();  //显示
     });
 
     
